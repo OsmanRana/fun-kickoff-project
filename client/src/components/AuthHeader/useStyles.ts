96371a2c97e06828f73bbc348498b7eff8fcccdc
OsmanRana <osmancmc@yahoo.com>
2022-01-10 20:58:40 +0600

    button fixed

--- client/src/components/AuthHeader/useStyles.ts
+++ client/src/components/AuthHeader/useStyles.ts
@@ -21,7 +21,6 @@ const useStyles = makeStyles((theme: Theme) => ({
     height: 54,
     borderRadius: theme.shape.borderRadius,
     filter: 'drop-shadow(0px 2px 6px rgba(74,106,149,0.2))',
-    color: 'white',
     boxShadow: 'none',
   },
 }));
