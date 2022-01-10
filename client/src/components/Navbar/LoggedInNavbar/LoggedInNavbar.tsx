d9f33056736071eb8ed5e2f642917be7265bd7f7
OsmanRana <osmancmc@yahoo.com>
2022-01-10 11:14:27 +0600

    logout implemented

--- client/src/components/Navbar/LoggedInNavbar/LoggedInNavbar.tsx
+++ client/src/components/Navbar/LoggedInNavbar/LoggedInNavbar.tsx
@@ -10,13 +10,17 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
+import { ListItemIcon, ListItemText, Divider } from '@mui/material';
+import { Person as ProfileIcon, Logout as LogoutIcon, Settings as SettingsIcon } from '@mui/icons-material';
+import { useAuth } from '../../../context/useAuthContext';
 
 const pages = ['My Jobs', 'Massages'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 const LoggedInNavbar = () => {
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
   const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const { loggedInUser, logout } = useAuth();
 
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
@@ -32,9 +36,16 @@ const LoggedInNavbar = () => {
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const handleLogout = () => {
+    handleClose();
+    logout();
+  };
 
   return (
-    <AppBar position="static">
+    <AppBar position="static" style={{ backgroundColor: 'white' }}>
       <Box sx={{ ml: 5, mr: 10 }}>
         <Toolbar disableGutters>
           <Box sx={{ mr: 2, my: 3, display: { xs: 'none', md: 'flex' } }}>
@@ -48,7 +59,7 @@ const LoggedInNavbar = () => {
               aria-controls="menu-appbar"
               aria-haspopup="true"
               onClick={handleOpenNavMenu}
-              color="inherit"
+              color="primary"
             >
               <MenuIcon />
             </IconButton>
@@ -72,7 +83,9 @@ const LoggedInNavbar = () => {
             >
               {pages.map((page) => (
                 <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
+                  <Typography sx={{ color: 'primary' }} textAlign="center">
+                    {page}
+                  </Typography>
                 </MenuItem>
               ))}
             </Menu>
@@ -80,9 +93,13 @@ const LoggedInNavbar = () => {
           <Box sx={{ mr: 2, my: 3, flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
             <img src="./logo.png" alt="Logo" />
           </Box>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex', justifyContent: 'right' } }}>
             {pages.map((page) => (
-              <Button key={page} onClick={handleCloseNavMenu} sx={{ my: 2, color: 'white', display: 'block' }}>
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, mr: 8, color: 'text.primary', display: 'block', textTransform: 'none' }}
+              >
                 {page}
               </Button>
             ))}
@@ -91,7 +108,10 @@ const LoggedInNavbar = () => {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                <Avatar
+                  alt="Remy Sharp"
+                  src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg"
+                />
               </IconButton>
             </Tooltip>
             <Menu
@@ -110,11 +130,25 @@ const LoggedInNavbar = () => {
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}
             >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
+              <MenuItem onClick={handleClose}>
+                <ListItemIcon>
+                  <SettingsIcon fontSize="small" />
+                </ListItemIcon>
+                <ListItemText>Settings</ListItemText>
+              </MenuItem>
+              <MenuItem onClick={handleClose}>
+                <ListItemIcon>
+                  <ProfileIcon fontSize="small" />
+                </ListItemIcon>
+                <ListItemText>Profile</ListItemText>
+              </MenuItem>
+              <Divider />
+              <MenuItem onClick={handleLogout}>
+                <ListItemIcon>
+                  <LogoutIcon fontSize="small" />
+                </ListItemIcon>
+                <ListItemText>Logout</ListItemText>
+              </MenuItem>
             </Menu>
           </Box>
         </Toolbar>
